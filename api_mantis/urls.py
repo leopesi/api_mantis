7a82874a0d3f5d9b40f6f6a4a61a4b2a3423d076
Leonardo Pereira <leopesi@yahoo.com.br>
2024-07-24 14:56:45 -0300

    Refact flack8

--- api_mantis/urls.py
+++ api_mantis/urls.py
@@ -2,8 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    #path('home', views.index, name='about'),
     path("issue/<int:issue_id>/", views.mantis_issue_view, name="mantis_issue"),
 
     ]
-# path('post/list/', views.PostListView.as_view(), name='post_list'),
