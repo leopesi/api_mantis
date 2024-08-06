a2cf2b66fb11f4ab302ef92ddbbc52e29453b51c
Leonardo Pereira <leopesi@yahoo.com.br>
2024-08-06 14:11:36 -0300

    added variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,41 @@ variable "region" {
   description = "The AWS region to create resources in."
   default     = "us-east-1"
 }
+
+# networking
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1b", "us-east-1c"]
+}
+
+# load balancer
+
+variable "health_check_path" {
+  description = "Health check path for the default target group"
+  default     = "/ping/"
+}
+
+# ecs
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  default     = "production"
+}
