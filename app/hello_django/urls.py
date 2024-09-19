32ef59c8beeb2e6a4b3254942349c0f5f3fb9acc
Leonardo Pereira <leopesi@yahoo.com.br>
2024-09-19 15:29:51 -0300

    Refactored

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
 from . import views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('ticket/', include('api.urls')),
+
+
 ]
+
--- app/hello_django/views.py
+++ app/hello_django/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Welcome to Hello Django!')
+    return HttpResponse('')
