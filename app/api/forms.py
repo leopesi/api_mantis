540794c47cb7a05d1554b771af3fd8104986e697
Leonardo Pereira <leopesi@yahoo.com.br>
2024-09-26 09:26:15 -0300

    Added Django Forms)

---
+++ app/api/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class MantisIssueForm(forms.Form):
+    issue_id = forms.CharField(
+        label="ID da Issue",
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Adiciona classes de estilo
+            'placeholder': 'Digite o ID da Issue',
+            'required': True
+        })
+    )
