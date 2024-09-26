1f8a7801e8e6ba018135e8e0a9d62f4b7d1d65c8
Leonardo Pereira <leopesi@yahoo.com.br>
2024-09-26 09:25:36 -0300

    Refact rotes to CBW)

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,15 @@
-from . import views
+from django.urls import path
+from .views import ConsultaMantisView, FetchAndAnalyzeMantisIssueView
+
+urlpatterns = [
+    path('consulta/', ConsultaMantisView.as_view(), name='mantis_consulta'),
+    path('<str:issue_id>/', FetchAndAnalyzeMantisIssueView.as_view(), name='mantis_issue'),
+]
+"""from . import views
 from django.urls import path
 
 urlpatterns = [
     path('consulta/', views.consulta_mantis_view, name='consulta_mantis'),
     path("<int:issue_id>/", views.mantis_issue_view, name="mantis_issue"),
 ]
+"""
--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -5,9 +5,9 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
     path("admin/", admin.site.urls),
-    path('ticket/', include('api.urls')),
+    path('', include('api.urls')),
 
 
 ]
